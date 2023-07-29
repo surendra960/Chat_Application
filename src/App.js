6a853617698eddccf665a67db9bb7d012ec834db
unknown <smeena1@es.iitr.ac.in>
2023-07-29 06:28:58 +0530

    Chat application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+
+const App = ()=>{
+    return (
+        <ChatEngine height = "100vh"
+        projectID="
+        9e68d6a4-fdf2-4104-92ec-80d1acb243fd"
+        userName="javascriptmastry"
+        userSecret="123123"
+        />
+    );
 }
 
 export default App;
